83f81014cfde91bfc91ff613c87ce1b9a4d3f489
Teo Stocco <teo@zifeo.com>
2016-12-21 16:39:13 +0100

    clean old command

--- src/main/scala/ch/epfl/telegram/InlineEpflBot.scala
+++ src/main/scala/ch/epfl/telegram/InlineEpflBot.scala
@@ -1,49 +1,14 @@
 package ch.epfl.telegram
 
-import java.net.URLEncoder
-
-import akka.http.scaladsl.Http
-import akka.http.scaladsl.model.{HttpRequest, Uri}
-import akka.http.scaladsl.unmarshalling.Unmarshal
-import akka.util.ByteString
-import info.mukel.telegrambot4s.Implicits._
 import info.mukel.telegrambot4s.api._
-import info.mukel.telegrambot4s.methods._
-import info.mukel.telegrambot4s.models._
 
 import scala.io.Source
 import scala.util.Properties
 
-object InlineEpflBot extends TelegramBot with Polling with Commands with ChatActions
+object InlineEpflBot extends App with TelegramBot with Polling with Commands with ChatActions
   with TL with Survey with InlineEpflDirectory with Events with Menu with Room {
 
   lazy val token = Properties.envOrNone("EPFLBOT_TOKEN").getOrElse(Source.fromFile("token").getLines().mkString)
-
-  val ttsApiBase = "http://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&tl=en-us&q="
-
-  on("/speak") { implicit msg => args =>
-    val text = args mkString " "
-    val url = ttsApiBase + URLEncoder.encode(text, "UTF-8")
-    for {
-      response <- Http().singleRequest(HttpRequest(uri = Uri(url)))
-      if response.status.isSuccess()
-      bytes <-  Unmarshal(response).to[ByteString]
-    } /* do */ {
-      uploadingAudio // hint the user
-      val voiceMp3 = InputFile("voice.mp3", bytes)
-      request(SendVoice(msg.sender, voiceMp3))
-
-      // Simple Java integration example for John.
-      reply(MyFunctions.hello())
-    }
-  }
-
-  on("/lmgtfy") { implicit msg => args =>
-    reply(
-      "http://lmgtfy.com/?q=" + URLEncoder.encode(args mkString " ", "UTF-8"),
-      disableWebPagePreview = true
-    )
-  }
 
   on("/about") { implicit msg => _ =>
     reply(
@@ -61,8 +26,6 @@ object InlineEpflBot extends TelegramBot with Polling with Commands with ChatAct
     )
   }
 
-  def main(args: Array[String]): Unit = {
-    //DirectoryScraper.getPersons("Sophia")
-    run()
-  }
+  run()
+
 }
