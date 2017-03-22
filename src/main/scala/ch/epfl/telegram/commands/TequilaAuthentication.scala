3df46a5207aa2276259066f8cc2e797039c80c23
Teo Stocco <teo@zifeo.com>
2017-03-22 17:44:23 +0100

    persist tequila information

--- src/main/scala/ch/epfl/telegram/commands/TequilaAuthentication.scala
+++ src/main/scala/ch/epfl/telegram/commands/TequilaAuthentication.scala
@@ -9,6 +9,7 @@ import akka.http.scaladsl.server.Directives
 import akka.http.scaladsl.unmarshalling.PredefinedFromEntityUnmarshallers._
 import akka.http.scaladsl.unmarshalling.Unmarshal
 import ch.epfl.telegram.Config
+import ch.epfl.telegram.models.EPFLUser
 import info.mukel.telegrambot4s.Implicits._
 import info.mukel.telegrambot4s.api.{BotBase, Commands, TelegramBot}
 import info.mukel.telegrambot4s.methods.{AnswerInlineQuery, GetMe}
@@ -18,8 +19,6 @@ import io.circe.generic.JsonCodec
 import scala.concurrent.Await
 import scala.concurrent.duration._
 import scala.language.postfixOps
-
-@JsonCodec case class EPFLUser(sciper: Int, firstName: String, name: String, email: String, gaspar: String, where: String)
 
 /**
   * EPFL's Tequila authentication, the process is as follows:
@@ -42,29 +41,32 @@ import scala.language.postfixOps
   * If the authentication succeeds, the user will be redirected to the "?start=success" sub-command
   * to confirm that the login was successful.
   */
-trait TequilaAuthentication extends BotBase with Commands {
-  _: TelegramBot =>
+trait TequilaAuthentication extends BotBase with Commands { _: TelegramBot =>
 
-  // TODO: Persist to database
-  val loggedUsers = scala.collection.mutable.Map[Int, EPFLUser]() // telegram_user_id -> sciper
   val tequilaToken = scala.collection.mutable.Map[String, Int]() // request_token -> telegram_user_id
 
-  on("/start") { implicit msg => {
-    case Seq("login") =>
-      startLogin
+  on("/start") { implicit msg =>
+    {
+      case Seq("login") =>
+        startLogin
 
-    case Seq("success") =>
-      // Greet the user
-      for (user <- msg.from) {
-        for (epflUser <- loggedUsers.synchronized { loggedUsers.get(user.id)})
-          reply(s"Hi, ${epflUser.firstName}!\n" +
-            "You are successfully authenticated with your EPFL account.\n" +
-            "@EPFLBot DO NOT store any sensible information such as passwords or personal data.",
-            replyMarkup = ReplyKeyboardRemove())
-      }
+      case Seq("success") =>
+        // Greet the user
+        for (user <- msg.from) {
+          EPFLUser.fromId(user.id).foreach {
+            case Some(EPFLUser(_, _, firstName, _, _, _, _)) =>
+              reply(
+                s"Hi, $firstName!\n" +
+                  "You are successfully authenticated with your EPFL account.\n" +
+                  "@EPFLBot DO NOT store any sensible information such as passwords or personal data.",
+                replyMarkup = ReplyKeyboardRemove()
+              )
+            case None =>
+          }
+        }
 
-    case _ => /* ignore */
-  }
+      case _ => /* ignore */
+    }
   }
 
   on("/login") { implicit msg => _ =>
@@ -73,68 +75,54 @@ trait TequilaAuthentication extends BotBase with Commands {
 
   on("/logout") { implicit msg => _ =>
     for (user <- msg.from) {
-      loggedUsers.synchronized {
-        loggedUsers -= user.id
+      for (_ <- EPFLUser.removeId(user.id)) {
         reply("Bye bye!")
       }
     }
   }
 
-
-  override def onInlineQuery(inlineQuery: InlineQuery): Unit = {
-
-    val epflUser = loggedUsers.synchronized {
-      loggedUsers.get(inlineQuery.from.id)
+  override def onInlineQuery(inlineQuery: InlineQuery): Unit =
+    EPFLUser.fromId(inlineQuery.from.id).map {
+      case Some(epflUser) =>
+        super.onInlineQuery(inlineQuery)
+      case None =>
+        // This only works on Telegram mobile (tested on Android)
+        // TODO: Report to Telegram support
+        request(
+          AnswerInlineQuery(inlineQuery.id,
+                            cacheTime = 1,
+                            switchPmText = "Connect to your EPFL account!",
+                            results = Seq.empty,
+                            switchPmParameter = "login")
+        )
     }
-
-    if (epflUser.isEmpty) {
-      // This only works on Telegram mobile (tested on Android)
-      // TODO: Report to Telegram support
-      request(
-        AnswerInlineQuery(
-          inlineQuery.id,
-          cacheTime = 1,
-          switchPmText = "Connect to your EPFL account!",
-          results = Seq.empty,
-          switchPmParameter = "login"))
-    } else
-      super.onInlineQuery(inlineQuery)
-  }
 
   def startLogin(implicit msg: Message): Unit = {
     for (user <- msg.from) {
-      val userIsLoggedIn = loggedUsers.synchronized {
-        loggedUsers.contains(user.id)
-      }
-      if (userIsLoggedIn)
-        reply("You are already logged in, please /logout first.")
-      else {
-        if (msg.chat.`type` != "private") {
-          val privateChatUrl = deepLink("login").toString()
+      if (msg.chat.`type` != "private") {
+        val privateChatUrl = deepLink("login").toString()
+        val button         = InlineKeyboardMarkup(Seq(Seq(InlineKeyboardButton("Let's do it!", url = privateChatUrl))))
 
-          val button = InlineKeyboardMarkup(Seq(Seq(
-            InlineKeyboardButton("Let's do it!", url = privateChatUrl))))
+        reply("Switch to private chat to login.", replyMarkup = button)
+      } else {
 
-          reply("Switch to private chat to login.", replyMarkup = button)
-        } else {
+        for {
+          req <- Http().singleRequest(createRequest)
+          res <- Unmarshal(req.entity).to[String]
+        } yield {
+          if (!res.startsWith("key=")) {
+            throw new Exception(s"bad tequila create request response: '$res'")
+          } else {
+            val key = res.drop(4).trim()
+            val uri = requestAuth(key)
 
-          for {
-            req <- Http().singleRequest(createRequest)
-            res <- Unmarshal(req.entity).to[String]
-          } yield {
-            if (!res.startsWith("key=")) {
-              throw new Exception(s"bad tequila create request response: '$res'")
-            } else {
-              val key = res.drop(4).trim()
-              val uri = requestAuth(key)
-
-              tequilaToken.synchronized {
-                tequilaToken(key) = user.id
-              }
-
-              reply(s"Open this link to authenticate:\n ${uri.toString()}", replyMarkup = ReplyKeyboardRemove())
+            tequilaToken.synchronized {
+              tequilaToken(key) = user.id
             }
+
+            reply(s"Open this link to authenticate:\n ${uri.toString()}", replyMarkup = ReplyKeyboardRemove())
           }
+
         }
       }
     }
@@ -142,28 +130,28 @@ trait TequilaAuthentication extends BotBase with Commands {
 
   import Directives._
 
-  val host = Config.tequila.host
+  val host             = Config.tequila.host
   val createRequestUri = s"$host/createrequest"
-  val requestAuthUri = s"$host/requestauth"
-  val fetchAttrsUri = s"$host/fetchattributes"
-  val tokenUri = "tequila"
+  val requestAuthUri   = s"$host/requestauth"
+  val fetchAttrsUri    = s"$host/fetchattributes"
+  val tokenUri         = "tequila"
 
   // TODO: Put this global in EPFLBot?
   private lazy val me = Await.result(request(GetMe), 10 seconds)
-  val botName = me.firstName
+  val botName         = me.firstName
 
   val deepLinkUri = s"https://t.me/$botName" // ?start=
 
   private val createRequestParams = {
     val interface = Config.http.interface
-    val port = Config.http.port
+    val port      = Config.http.port
 
     val params = Map(
       "urlaccess" -> s"http://$interface:$port/$tokenUri",
-      "service" -> Config.tequila.service,
-      "request" -> Config.tequila.request,
-      "require" -> Config.tequila.require,
-      "allows" -> Config.tequila.allows
+      "service"   -> Config.tequila.service,
+      "request"   -> Config.tequila.request,
+      "require"   -> Config.tequila.require,
+      "allows"    -> Config.tequila.allows
     )
     tequilaSerialize(params)
   }
@@ -183,17 +171,21 @@ trait TequilaAuthentication extends BotBase with Commands {
     RequestBuilding.Post(fetchAttrsUri, s"key=$key")
 
   def tequilaSerialize(data: Map[String, String]): String =
-    data.foldLeft("") { case (content, (key, value)) =>
-      s"$content\n$key=$value"
+    data.foldLeft("") {
+      case (content, (key, value)) =>
+        s"$content\n$key=$value"
     }
 
   def tequilaDeserialize(data: String): Map[String, String] =
-    data.split('\n').flatMap { entry =>
-      entry.split('=').toList match {
-        case k :: v :: Nil => Some(k -> v)
-        case _ => None
+    data
+      .split('\n')
+      .flatMap { entry =>
+        entry.split('=').toList match {
+          case k :: v :: Nil => Some(k -> v)
+          case _             => None
+        }
       }
-    }.toMap
+      .toMap
 
   val routes =
     pathPrefix(tokenUri) {
@@ -204,18 +196,18 @@ trait TequilaAuthentication extends BotBase with Commands {
         } yield {
           val data = tequilaDeserialize(res)
 
-          val epflUser = EPFLUser(
-            sciper = data("uniqueid").toInt,
-            firstName = data("firstname"),
-            name = data("name"),
-            email = data("email"),
-            gaspar = data("user"),
-            where = data("where"))
-
-          loggedUsers.synchronized {
-            tequilaToken.synchronized {
-              for (token <- tequilaToken.get(key))
-                loggedUsers += (token -> epflUser)
+          tequilaToken.synchronized {
+            for (userId <- tequilaToken.get(key)) {
+              val epflUser = EPFLUser(
+                id = userId,
+                sciper = data("uniqueid").toInt,
+                firstName = data("firstname"),
+                name = data("name"),
+                email = data("email"),
+                gaspar = data("user"),
+                where = data("where")
+              )
+              EPFLUser.putUser(epflUser)
             }
           }
 
@@ -232,4 +224,3 @@ trait TequilaAuthentication extends BotBase with Commands {
     Http().bindAndHandle(routes, "::0", 8080)
   }
 }
-
--- src/main/scala/ch/epfl/telegram/models/Reaction.scala
+++ src/main/scala/ch/epfl/telegram/models/Reaction.scala
@@ -6,23 +6,23 @@ import scala.concurrent.{ExecutionContext, Future}
 
 object Reaction {
 
-  val surveyStore   = "survey" / "welcome"
-  val feedbackStore = "feedback" / "welcome"
+  val surveyIndex   = "survey" / "welcome"
+  val feedbackIndex = "feedback" / "welcome"
 
   def putAnswer(userId: Long, responses: Map[String, String])(implicit ec: ExecutionContext): Future[Unit] =
     es.execute {
-      update(userId) in surveyStore docAsUpsert responses
+      update(userId) in surveyIndex docAsUpsert responses
     } map (_ => ())
 
   def getAnswers(userId: Long)(implicit ec: ExecutionContext): Future[Map[String, String]] = {
     es.execute {
-      get(userId) from surveyStore
+      get(userId) from surveyIndex
     } map (_.sourceAsMap.mapValues(_.toString))
   }
 
   def putFeedback(userId: Long, feedback: String)(implicit ec: ExecutionContext): Future[Unit] =
     es.execute {
-      indexInto(feedbackStore) fields ("user" -> userId, "feedback" -> feedback)
+      indexInto(feedbackIndex) fields ("user" -> userId, "feedback" -> feedback)
     } map (_ => ())
 
 }
