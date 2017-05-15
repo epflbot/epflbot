6b520242c1831cc7e064f9864022cfc02de44acc
Teo Stocco <teo@zifeo.com>
2017-05-16 00:37:07 +0200

    fix docker binding

--- src/main/scala/ch/epfl/telegram/Microsite.scala
+++ src/main/scala/ch/epfl/telegram/Microsite.scala
@@ -7,7 +7,8 @@ trait Microsite extends WebRoutes {
 
   import akka.http.scaladsl.server.Directives._
 
-  val port = Config.http.port
+  val port                 = Config.http.port
+  override val interfaceIp = "0.0.0.0"
 
   override def routes: Route =
     super.routes ~
